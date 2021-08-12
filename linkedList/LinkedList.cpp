14d906e070ffd81825224b9612092fddde2b4069
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2021-08-13 00:01:45 +0530

    Linked List

---
+++ linkedList/LinkedList.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *next;
+
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+void insertAtTail(node *&head, int val)
+{
+    node *n = new node(val);
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+void insertAtHead(node *&head, int val)
+{
+    node *n = new node(val);
+    n->next = head;
+    head = n;
+}
+void display(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+bool search(node *head, int key)
+{
+
+    while (head != NULL)
+    {
+        if (head->data == key)
+        {
+            return true;
+        }
+        head = head->next;
+    }
+    return false;
+}
+
+void delFirst(node* &head){
+    node* todelete=head;
+    head=head->next;
+    delete todelete;
+}
+void del(node* &head,int val){
+    if(head==NULL){
+        return ;
+    }
+    if(head->data==val){
+        delFirst(head);
+        return;
+    }
+    // if(head->next==NULL){
+    //     delFirst(head);
+    //     return ;
+    // }
+    node* temp=head;
+    while(temp->next->data!=val){
+        temp=temp->next;
+    }
+    node* todelete=temp->next;
+    temp->next=temp->next->next;
+
+    delete todelete;
+}
+int main()
+{
+    node *head = NULL;
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    insertAtTail(head, 4);
+    display(head);
+    cout << search(head, 1) << endl;
+    del(head,1);
+    cout << "after deleting the item" << endl;
+    display(head);
+
+    return 0;
+}
