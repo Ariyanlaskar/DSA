5e4b771ee5a3932a488abff7f29ec96f54778b4f
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2021-08-25 23:49:30 +0530

    Graph

---
+++ graph/BFS.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N=1e5+2;
+bool vis[N];
+vector<int>adj[N];
+int main(){
+    
+    for(int i=0;i<N;i++){
+        vis[i]=0;
+    }
+    int n,m;
+    cin>>n>>m;
+    int x,y;
+    for(int i=0;i<m;i++){
+        cin>>x>>y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+    queue<int>q;
+    q.push(1);
+    vis[1]=true;
+    while(!q.empty()){
+        int node=q.front();
+        q.pop();
+        cout<<node<<endl;
+        vector<int>::iterator it;
+        for(auto it:adj[node]){
+            if(!vis[it]){
+                q.push(it);
+                vis[it]=1;
+            }
+        }
+    }
+    return 0;
+    
+
+}
