c8e0ee3433009455e0dfe0b6d5b220e3d97180ce
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2021-09-17 23:03:51 +0530

    LeetCode

--- LeetCode/Graph/FibonacciSequence.cpp
+++ LeetCode/Graph/FibonacciSequence.cpp
@@ -7,6 +7,24 @@ int fib(int n) {
         }
         return DP[n];
     }
+//Alternnative Solution Using Memorizing Concept(Slow Appraoch Than The Above One)
+//int fib(int n) {
+//         int Memo[31];
+//         for(int i=0;i<31;i++){
+//             Memo[i]=-1;
+//         }
+//         if(n==0){
+//             return 0;
+//         }
+//         if(n==1){
+//             return 1;
+//         }
+//         if(Memo[n]!=-1){
+//             return Memo[n];
+//         }
+//         Memo[n]=fib(n-1)+fib(n-2);
+//         return Memo[n];
+//     }
 int main(){
     int n=27;
     cout<<fib(n)<<endl;
