b4757df04b735de4af90a7a86e175853be165580
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2021-09-18 22:23:57 +0530

    DP

--- DP/MinimumNumberOfSqures.cpp
+++ DP/MinimumNumberOfSqures.cpp
@@ -10,8 +10,19 @@ int minSquare(int n){
     return ans+1;
     
 }
-// //Using Memorization;
-// int minSquare(int n)
+//Using Memorization->Unable to produce result for large numbers like '1000';
+// const int INF=INT_MAX;
+// int minSquare(int n){
+//     if(n==0 || n==1 || n==2 || n==3){
+//         return n;
+//     }
+//     int ans=INT_MAX;
+//     for(int i=1;i*i<=n;i++){
+//         ans=min(ans, 1 + minSquare(n-i*i));
+//     }
+//     return ans;
+// }
+
 int main(){
     int n;
     cin>>n;
