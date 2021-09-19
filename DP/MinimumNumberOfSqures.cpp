1b52728b37d68b6490474f19df46ac93663fa2f0
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2021-09-19 19:23:48 +0530

    DP

--- DP/MinimumNumberOfSqures.cpp
+++ DP/MinimumNumberOfSqures.cpp
@@ -1,16 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 //Simple Approach;
-int minSquare(int n){
-    if(n==0 || n==1 || n==2 || n==3){
-        return n;
-    }
-    int x=sqrt(n);
-    int ans=n-x*x;
-    return ans+1;
+// int minSquare(int n){
+//     if(n==0 || n==1 || n==2 || n==3){
+//         return n;
+//     }
+//     int x=sqrt(n);
+//     int ans=n-x*x;
+//     return ans+1;
     
-}
-//Using Memorization->Unable to produce result for large numbers like '1000';
+// }
+// //Burte force(Using Recursion)->Unable to produce result for large numbers like '1000';
 // const int INF=INT_MAX;
 // int minSquare(int n){
 //     if(n==0 || n==1 || n==2 || n==3){
@@ -22,8 +22,26 @@ int minSquare(int n){
 //     }
 //     return ans;
 // }
+// //Using DP->Memoziation
+const int N=1e5+2,MOD=1e9+7;
+int Dp[N];
+int minSquare(int n){
+    if(n==0 || n==1 || n==2 || n==3){   
+        return n;
+    }
+    if(Dp[n]!=MOD){
+        return Dp[n];
+    }
+    for(int i=1;i*i<=n;i++){
+        Dp[n]=min(Dp[n], 1 + minSquare(n-i*i));
+    }
+    return Dp[n];
+}
 
 int main(){
+    for(int i=0;i<N;i++){
+        Dp[i]=MOD;
+    }
     int n;
     cin>>n;
     cout<<minSquare(n)<<endl;
