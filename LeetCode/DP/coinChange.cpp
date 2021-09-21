42d6329b3fab71fcea0273bd258d2b3d331acfbd
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2021-09-21 13:50:17 +0530

    LeetCode

---
+++ LeetCode/DP/coinChange.cpp
@@ -0,0 +1,28 @@
+//LeetCode Medium 322;
+#include<bits/stdc++.h>
+using namespace std;
+int coinChange(vector<int>& coins, int amount) {
+    long long int dp[amount+1];
+    for(int i=0;i<=amount;i++){
+        dp[i]=INT_MAX;
+    }
+    dp[0]=0;
+    for(int j=1;j<=amount;j++){
+        for(int i=0;i<coins.size();i++){
+            if(coins[i]<=j){
+                int rem=j-coins[i];
+                dp[j]=min(dp[j],dp[rem]+1);
+            }
+        }
+    }
+    if(dp[amount]==INT_MAX){
+        return -1;
+    }
+    return dp[amount];
+}
+int main(){
+    vector<int>coins={1,2,5};
+    int amount=100;
+    cout<<coinChange(coins,amount);
+    return 0;
+}
