027961bc40d981371065397fd34466f18bbcbf85
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2021-09-22 16:30:34 +0530

    Dp

--- DP/01Knapsack.cpp
+++ DP/01Knapsack.cpp
@@ -1,21 +1,48 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+//USING DP -> MEMOIZATION APPROACH;
+const int N=1e3+2;
+int dp[N][N];
 int Knapsack(int n,vector<int>&values,vector<int>&wt,int w){
     if(n<=0 || w<=0){
         return 0;
     }
-    if(wt[n-1]>w){
-        return Knapsack(n-1,values,wt,w);
+    if(dp[n][w]!=-1){
+        return dp[n][w];
     }
-    return max(Knapsack(n-1,values,wt,w),Knapsack(n-1,values,wt,w-wt[n-1])+ values[n-1]);
+    if(wt[n-1]>w){
+        dp[n-1][w]=Knapsack(n-1,values,wt,w);
+    }
+    else{
+        dp[n][w]=max(Knapsack(n-1,values,wt,w),Knapsack(n-1,values,wt,w-wt[n-1])+values[n-1]);
+    }
+    return dp[n][w];
+    
 }
+// Simple Solution(not for larger values); 
+// int Knapsack(int n,vector<int>&values,vector<int>&wt,int w){
+//     if(n<=0 || w<=0){
+//         return 0;
+//     }
+//     if(wt[n-1]>w){
+//         return Knapsack(n-1,values,wt,w);
+//     }
+//     return max(Knapsack(n-1,values,wt,w),Knapsack(n-1,values,wt,w-wt[n-1])+ values[n-1]);
+// }
+
 int main(){
     int n; // n -> number of items;
     n=3;
-    vector<int>values={60,100,150};
-    vector<int>wt={15,30,45};
+    vector<int>values={60,100,150};// values of each item;
+    vector<int>wt={15,30,45};// weight of each item;
     int w; // w -> weight of knapsack;
     w=50;
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            dp[i][j]=-1;
+        }
+    }
     cout<<Knapsack(n,values,wt,w)<<endl;
     
     return 0;
