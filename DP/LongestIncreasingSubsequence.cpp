21031c637e348ef86a77c23cf1f33dcb1f528b47
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2021-09-24 14:16:00 +0530

    DP

--- DP/LongestIncreasingSubsequence.cpp
+++ DP/LongestIncreasingSubsequence.cpp
@@ -1,25 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
-int lengthOfLIS(vector<int>&nums) {
-    vector<int>dp(nums.size(),1);
-
-    for(int i=0;i<nums.size();i++)
-    {
-        for(int j=0;j<i;j++)
-        {
-            if(nums[i]>nums[j])
-            {
+int lengthOfLIS(vector<int>&arr) {
+    int n=arr.size();
+    vector<int>dp(n,1);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            if(arr[i]>arr[j]){
                 dp[i]=max(dp[i],dp[j]+1);
             }
         }
     }
     int ans=0;
-    for(int i=0;i<nums.size();i++)
-        ans = max(ans,dp[i]);
+    for(int i=0;i<n;i++){
+        ans=max(ans,dp[i]);
+    }
     return ans;
 }
 int main(){
-    vector<int>nums={1,3,6,7,9,4,10,5,6};
-    cout<<lengthOfLIS(nums);
+    vector<int>arr={1,3,6,7,9,4,10,5,6};
+    cout<<lengthOfLIS(arr);
     return 0;
 }
