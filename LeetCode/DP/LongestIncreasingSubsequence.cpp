2e8824e1242274c5d8021c1b8dd6a4deb87e9fdd
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2021-09-25 12:33:17 +0530

    LeetCode

--- LeetCode/DP/LongestIncreasingSubsequence.cpp
+++ LeetCode/DP/LongestIncreasingSubsequence.cpp
@@ -1,15 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 int lengthOfLIS(vector<int>&nums){
-    int n=nums.size();
-    vector<int>dp(n,1);
-    for(int i=0;i<n;i++){
-        for(int j=0;j<i;j++){
-            if(nums[i]>nums[j]){
-                dp[i]=max(dp[i],dp[j]+1);
-            }
+int n=nums.size();
+vector<int>dp(n,1);
+for(int i=0;i<n;i++){
+    for(int j=0;j<i;j++){
+        if(nums[i]>nums[j]){
+            dp[i]=max(dp[i],dp[j]+1);
         }
     }
+}
     int ans=0;
     for(int i=0;i<n;i++){
         ans=max(ans,dp[i]);
