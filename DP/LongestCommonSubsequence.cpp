f4959f4051e0063d1d48458749b042695546e1b2
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2021-09-25 14:03:09 +0530

    DP

--- DP/LongestCommonSubsequence.cpp
+++ DP/LongestCommonSubsequence.cpp
@@ -1,31 +1,52 @@
-//Using DP->Memoization;
+//Using DP->Tabulation(bottom-Up);
 #include<bits/stdc++.h>
 using namespace std;
-const int N=1e3+2;
-int dp[N][N];
+
+// //using DP->Memoiztaion;
+// const int N=1e3+2;
+// int dp[N][N];
 int LCS(string s1,string s2,int n,int m){
-    if(n==0 || m==0){
-        return 0;
-    }
-    if(dp[n][m]!=-1){
-        return dp[n][m];
-    }
-    if(s1[n-1]==s2[m-1]){
-        dp[n][m]=1+LCS(s1,s2,n-1,m-1);
-    }
-    else{
-        dp[n][m]=max(LCS(s1,s2,n-1,m),LCS(s1,s2,n,m-1));
+    vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<m+1;j++){
+            if(i==0 || j==0){
+                dp[i][j]=0;
+                continue;
+            }
+            if(s1[n-1]==s2[m-1]){
+                dp[i][j]=1+dp[i-1][j-1];
+            }
+            else{
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
     }
     return dp[n][m];
 }
+// int LCS(string s1,string s2,int n,int m){
+//     if(n==0 || m==0){
+//         return 0;
+//     }
+//     if(dp[n][m]!=-1){
+//         return dp[n][m];
+//     }
+//     if(s1[n-1]==s2[m-1]){
+//         dp[n][m]=1+LCS(s1,s2,n-1,m-1);
+//     }
+//     else{
+//         dp[n][m]=max(LCS(s1,s2,n-1,m),LCS(s1,s2,n,m-1));
+//     }
+//     return dp[n][m];
+// }
+
 int main(){
     int n=5;
     int m=3;
-    for(int i=0;i<N;i++){
-        for(int j=0;j<N;j++){
-            dp[i][j]=-1;
-        }
-    }
+    // for(int i=0;i<N;i++){
+    //     for(int j=0;j<N;j++){
+    //         dp[i][j]=-1;
+    //     }
+    // }
     string s1="abcde";
     string s2="ace";
     cout<<LCS(s1,s2,n,m)<<endl;
