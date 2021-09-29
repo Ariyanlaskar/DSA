7e7dad6f574e675803d65158b33c3143476f4057
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2021-09-29 19:30:02 +0530

    DP

--- DP/OptimalGameStrategy.cpp
+++ DP/OptimalGameStrategy.cpp
@@ -1,5 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
+// // using DP->Memoization;
+int dp[100][100];
 int Solve(vector<int>&arr,int start,int end){
     if(start==end){
         return arr[start];
@@ -7,13 +9,32 @@ int Solve(vector<int>&arr,int start,int end){
     if(start>end){
         return 0;
     }
-    int l=arr[start]+min(Solve(arr,arr[start+2],end),Solve(arr,arr[start+1],end-1));
+    if(dp[start][end]!=-1){
+        return dp[start][end];
+    }
+    int l=arr[start]+min(Solve(arr,arr[start]+1,end),Solve(arr,arr[start+1],end-1));
     int r=arr[end]+min(Solve(arr,arr[start+1],end-1),Solve(arr,arr[start],end-2));
-    return max(l,r);
+    return dp[l][r]=max(l,r);
 }
+// int Solve(vector<int>&arr,int start,int end){
+//     if(start==end){
+//         return arr[start];
+//     }
+//     if(start>end){
+//         return 0;
+//     }
+//     int l=arr[start]+min(Solve(arr,arr[start+2],end),Solve(arr,arr[start+1],end-1));
+//     int r=arr[end]+min(Solve(arr,arr[start+1],end-1),Solve(arr,arr[start],end-2));
+//     return max(l,r);
+// }
 int main(){
     int n;
     cin>>n;
+    for(int i=0;i<101;i++){
+        for(int j=0;j<101;j++){
+            dp[i][j]=-1;
+        }
+    }
     vector<int>arr(n);
     for(int i=0;i<n;i++){
         cin>>arr[i];
