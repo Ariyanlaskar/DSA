7e8ead40071156ee6e55b515cad32928329772e3
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2021-10-02 20:20:50 +0530

    LeetCode

--- LeetCode/String/CountBinarySubstrings.cpp
+++ LeetCode/String/CountBinarySubstrings.cpp
@@ -1,24 +1,48 @@
 //LeetCode Easy 696;
 #include<bits/stdc++.h>
 using namespace std;
+// //TC - O(N);
+// //SC - O(N);
+// int countBinarySubstrings(string s){
+//     int prev_grp=0;
+//     int curr_grp=1;
+//     int ans=0;
+//     for(int i=1;i<s.length();i++){
+//         if(s[i]!=s[i-1]){
+//             ans+=min(prev_grp,curr_grp);
+//             prev_grp=curr_grp;
+//             curr_grp=1;
+//         }
+//         else{
+//             curr_grp++;
+//         }
+//     }
+//     return ans+min(prev_grp,curr_grp);
+// }
+// // TC - O(N);
+// // SC - O(1);
 int countBinarySubstrings(string s){
-    int prev_grp=0;
-    int curr_grp=1;
-    int ans=0;
-    for(int i=1;i<s.length();i++){
+    int n=s.length();
+    int idx=0;
+    vector<int>grp(n);
+    grp[0]=1;
+    for(int i=1;i<n;i++){
         if(s[i]!=s[i-1]){
-            ans+=min(prev_grp,curr_grp);
-            prev_grp=curr_grp;
-            curr_grp=1;
+            idx++;
+            grp[idx]=1;
         }
         else{
-            curr_grp++;
+            grp[idx]++;
         }
     }
-    return ans+min(prev_grp,curr_grp);
+    int ans=0;
+    for(int i=1;i<n;i++){
+        ans+=min(grp[i],grp[i-1]);
+    }
+    return ans;
 }
 int main(){
-    string s = "00110011";
+    string s = "10101";
     cout<<countBinarySubstrings(s)<<endl;
     return 0;
 }
