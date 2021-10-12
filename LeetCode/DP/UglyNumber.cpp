935b751654102ab4b3a47e70d141d0feb12d96c5
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2021-10-12 14:00:27 +0530

    LeetCode

---
+++ LeetCode/DP/UglyNumber.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int uglyNumber(int n){
+    vector<int>dp(n+1);
+    int c2=0;
+    int c3=0;
+    int c5=0;
+    dp[0]=1;
+    for(int i=1;i<=n;i++){
+        dp[i]=min({2*dp[c2],3*dp[c3],5*dp[c5]});
+        if(dp[i]==2*dp[c2]){
+            c2++;
+        }
+        if(dp[i]==3*dp[c3]){
+            c3++;
+        }
+        if(dp[i]==5*dp[c5]){
+            c5++;
+        }
+    }
+    return dp[n-1];
+}
+int main(){
+    int n=10;
+    cout<<uglyNumber(n)<<endl;
+    return  0;
+}
