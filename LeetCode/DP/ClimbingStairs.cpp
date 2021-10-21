ed2652e8cbec9f9090ea6cf878d3c87107ca7de9
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2021-10-21 20:03:58 +0530

    LeetCode

---
+++ LeetCode/DP/ClimbingStairs.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int climbStairs(int n) {
+    vector<int>dp(n+1,0);
+    dp[0]=1;
+    dp[1]=1;
+    for(int i=2;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
+    }
+    return dp[n];
+}
+int main(){
+    int n=2;
+    cout<<climbStairs(n)<<endl;
+    return 0;
+}
