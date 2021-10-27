d913e29cba64a66d6be4b61f610d4d20ae758504
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2021-10-27 20:57:40 +0530

    LeetCode

---
+++ LeetCode/binaryTree.cpp/BinaryTreeInorderTraversal.cpp
@@ -0,0 +1,20 @@
+//Only Solution Function;
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> inorderTraversal(TreeNode* root) {
+    vector<int>ans;
+    findInorder(root,ans);
+    return ans;
+}
+void findInorder(TreeNode* root,vector<int>&ans){
+    if(root==NULL){
+        return;
+    }
+    findInorder(root->left,ans);
+    ans.push_back(root->val);
+    findInorder(root->right,ans);
+}
+int main(){
+    
+    return 0;
+} 
