c8840ee2e1bdc0b6b786606f77d020309aa260f2
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2021-10-30 11:18:32 +0530

    LeetCod

---
+++ LeetCode/BInaryTree&BST/UnivaluedBinaryTree.cpp
@@ -0,0 +1,19 @@
+//LeetCode Easy 965;
+//Only Function(Solution);
+#include<bits/stdc++.h>
+using namespace std;
+bool check(TreeNode* root,int value){
+    if(root==NULL){
+        return true;
+    }
+    if(root->val!=value){
+        return false;
+    }
+    return check(root->left,value) & check(root->right,value);
+}
+bool isUnivalTree(TreeNode* root) {
+    return check(root,root->val);
+}
+int main(){
+    return 0;
+}
