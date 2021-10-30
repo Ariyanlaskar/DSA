6ba2366c6bf4c8eecf5394323dccba068422e9f1
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2021-10-30 11:15:21 +0530

    LeetCode

---
+++ LeetCode/BInaryTree&BST/SymmetricTree.cpp
@@ -0,0 +1,22 @@
+//LeetCode Easy 101
+//Only Function(Solution);
+#include<bits/stdc++.h>
+using namespace std;
+bool isSymmetric(TreeNode* root) {
+    if(root==NULL){
+        return true;
+    }
+    return Symmetric_Or_Not(root->left,root->right);
+}
+bool Symmetric_Or_Not(TreeNode* root1,TreeNode* root2){
+    if(root1==NULL || root2==NULL){
+        return root1==root2;
+    }
+    if(root1->val!=root2->val){
+        return false;
+    }
+    return Symmetric_Or_Not(root1->left,root2->right) & Symmetric_Or_Not(root1->right,root2->left);
+}
+int main(){
+    return 0;
+}
