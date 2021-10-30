248a998c03f6f6c01af238ecaf0af911d7a930fb
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2021-10-30 11:35:11 +0530

    LeetCode

---
+++ LeetCode/BInaryTree&BST/BinaryTreePreorderTraversal.cpp
@@ -0,0 +1,20 @@
+//LeetCode Easy 144
+//Only Function(Solution);
+#include<bits/stdc++.h>
+using namespace std;
+vector<int>ans;
+void findPreorder(TreeNode* root){
+    if(root==NULL){
+        return;
+    }
+    ans.push_back(root->val);
+    findPreorder(root->left);
+    findPreorder(root->right);
+}
+vector<int> preorderTraversal(TreeNode* root) {
+    findPreorder(root);
+    return ans;
+}
+int main(){
+    return 0;
+}
