dab6d5478eb6746b52b9dd8c4df3cbf371dce2f3
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2021-11-05 08:54:20 +0530

    LeetCode

---
+++ LeetCode/BinarySearch/ArrangingCoins.cpp
@@ -0,0 +1,26 @@
+//LeetCode Easy 441;
+#include<bits/stdc++.h>
+using namespace std;
+int arrangeCoins(int n) {
+    int start=0;
+    int end=n;
+    while(start<=end){
+        long long int mid=(start+end)/2;
+        long long int res=mid*(mid+1)/2;
+        if(res==n){
+            return mid;
+        }
+        else if(res>n){
+            end=mid-1;
+        }
+        else{
+            start=mid+1;
+        }
+    }
+    return end;
+}
+int main(){
+    int n=5;
+    cout<<arrangeCoins(n)<<endl;
+    return 0;
+}
