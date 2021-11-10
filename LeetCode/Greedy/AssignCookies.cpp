66e4b625e49939ffdf4cfcaea3b1b1997332819e
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2021-11-10 19:11:33 +0530

    LeetCode

---
+++ LeetCode/Greedy/AssignCookies.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int findContentChildren(vector<int> &g, vector<int> &s)
+{
+    sort(g.begin(), g.end());
+    sort(s.begin(), s.end());
+    int i = 0;
+    int j = 0;
+    int count = 0;
+    while (i < g.size() && j < s.size())
+    {
+        if (g[i] <= s[j])
+        {
+            count++;
+            i++;
+            j++;
+        }
+        else if (g[i] > s[j])
+        {
+            j++;
+        }
+    }
+    return count;
+}
+int main()
+{
+    vector<int>g = {1,2,3}, s = {1,1};
+    cout<<findContentChildren(g,s);
+    return 0;
+}
+// Output : 1;
