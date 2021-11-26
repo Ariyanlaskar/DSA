ca8001d268c6eab3a74e2b8f459b93413b89614b
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2021-11-26 08:32:33 +0530

    LeetCode

---
+++ LeetCode/Hashing/FindtheDifference.cpp
@@ -0,0 +1,31 @@
+//LeetCode Easy 389;
+#include <bits/stdc++.h>
+using namespace std;
+char findTheDifference(string s, string t)
+{
+    map<char, int> mp;
+    for (auto &i : s)
+    {
+        mp[i]++;
+    }
+    for (auto &c : t)
+    {
+        mp[c]++;
+    }
+    for (auto it : mp)
+    {
+        if (it.second % 2 != 0)
+        {
+            return it.first;
+        }
+    }
+    return 0;
+}
+int main()
+{
+    string  s = "abcd", t = "abcde";
+    cout<<findTheDifference(s,t)<<endl;
+    return 0;
+}
+// Output: "e"
+// Explanation: 'e' is the letter that was added.
