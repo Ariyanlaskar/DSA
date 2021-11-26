c778014f1df571b0e7a7875d5da59d11bc91e755
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2021-11-26 12:19:34 +0530

    LeetCode

---
+++ LeetCode/Hashing/RansomeNote.cpp
@@ -0,0 +1,32 @@
+//LeetCode Easy 383;
+#include <bits/stdc++.h>
+using namespace std;
+bool canConstruct(string ransomNote, string magazine)
+{
+    if (ransomNote.size() > magazine.size())
+    {
+        return false;
+    }
+    unordered_map<char, int> Ran, Mag;
+    for (auto &c : magazine)
+    {
+        Mag[c]++;
+    }
+    for (auto &it : ransomNote)
+    {
+        if (Mag[it] == 0)
+        {
+            return false;
+        }
+        Mag[it]--;
+    }
+    return true;
+}
+int main()
+{
+    string ransomNote = "a", magazine = "b";
+    cout<<canConstruct(ransomNote,magazine)<<endl;
+    return 0;
+}
+// Input: ransomNote = "a", magazine = "b"
+// Output: false
