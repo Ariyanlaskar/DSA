d1f562d62f85a9a13670b68aff0edad51543c5b3
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2021-11-29 22:08:48 +0530

    LeetCode

---
+++ LeetCode/DP/maximumProductSubarray.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int maxProduct(vector<int> &nums)
+{
+    int n = nums.size();
+    vector<int> maxdp(n);
+    vector<int> mindp(n);
+    maxdp[0] = nums[0];
+    mindp[0] = nums[0];
+    for (int i = 1; i < n; i++)
+    {
+        maxdp[i] = max(maxdp[i - 1] * nums[i], max(mindp[i - 1] * nums[i], nums[i]));
+        mindp[i] = min(mindp[i - 1] * nums[i], min(maxdp[i - 1] * nums[i], nums[i]));
+    }
+    int ans = INT_MIN;
+    for (auto &n : maxdp)
+    {
+        ans = max(ans, n);
+    }
+    return ans;
+}
+int main()
+{
+    vector<int>nums = {2,3,-2,4};
+    cout<<maxProduct(nums)<<endl;
+    return 0;
+}
+// Input: nums = [2,3,-2,4]
+// Output: 6
+// Explanation: [2,3] has the largest product 6.
