892f0f9bb42ad16477be8350a4409c7f492c4ccd
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2021-12-24 20:39:42 +0530

    LeetCode

--- LeetCode/LinkedList/ReorderList.cpp
+++ LeetCode/LinkedList/ReorderList.cpp
@@ -1,33 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-ListNode *solve(ListNode *head)
-{
-    if (head->next == NULL || head->next->next == NULL)
-        return head;
-
-    ListNode *first = head;
-    ListNode *last = head;
-    ListNode *prevlast = NULL;
-    ListNode *nextHead = head->next;
-
-    while (last->next != NULL)
-    {
-        prevlast = last;
-        last = last->next;
-    }
-
-    prevlast->next = NULL;
-    first->next = last;
-
-    last->next = solve(nextHead);
-
-    return first;
-}
-
 void reorderList(ListNode *head)
 {
-
-    solve(head);
+    if (head == NULL || head->next == NULL || head->next->next == NULL)
+    {
+        return;
+    }
+    int size = 0;
+    stack<ListNode *> st;
+    ListNode *temp = head;
+    while (temp != NULL)
+    {
+        size++;
+        st.push(temp);
+        temp = temp->next;
+    }
+    temp = head;
+    ListNode *dummy = new ListNode(-1);
+    ListNode *t = dummy;
+    t->next = head;
+    for (int i = 0; i < size / 2; i++)
+    {
+        ListNode *v = st.top();
+        st.pop();
+        v->next = head->next;
+        head->next = v;
+        head = head->next->next;
+    }
+    head->next = NULL;
+    head = dummy->next;
 }
 // Input: head = [1,2,3,4]
 // Output: [1,4,2,3]
