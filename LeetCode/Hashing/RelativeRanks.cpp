810b5b644ffe6ff7783634d5f07baea2804c79ed
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2021-12-29 15:23:59 +0530

    LeetCode

---
+++ LeetCode/Hashing/RelativeRanks.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<string> findRelativeRanks(vector<int> &score)
+{
+    vector<string> ans;
+    vector<int> s = score;
+    sort(score.begin(), score.end());
+    int j = 1;
+    map<int, string> mp;
+    for (int i = score.size() - 1; i >= 0; i--)
+    {
+        if (j == 1)
+        {
+            mp[score[i]] = "Gold Medal";
+            j++;
+        }
+        else if (j == 2)
+        {
+            mp[score[i]] = "Silver Medal";
+            j++;
+        }
+        else if (j == 3)
+        {
+            mp[score[i]] = "Bronze Medal";
+            j++;
+        }
+        else
+        {
+            mp[score[i]] = to_string(j);
+            j++;
+        }
+    }
+    for (int i = 0; i < score.size(); i++)
+    {
+        ans.push_back(mp[s[i]]);
+    }
+    return ans;
+}
+//Input: score = [5,4,3,2,1]
+// Output: ["Gold Medal","Silver Medal","Bronze Medal","4","5"]
