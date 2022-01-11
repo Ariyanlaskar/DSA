37b58350ac85a97570e46329f248fee3beb948a9
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-01-11 19:39:01 +0530

    LeetCode

--- LeetCode/BInaryTree&BST/SumOfRootToLeafBinaryNumbers.cpp
+++ LeetCode/BInaryTree&BST/SumOfRootToLeafBinaryNumbers.cpp
@@ -25,3 +25,38 @@ void findSum(TreeNode *root, int sum)
 // Input: root = [1,0,1,0,1,0,1]
 // Output: 22
 // Explanation: (100) + (101) + (110) + (111) = 4 + 5 + 6 + 7 = 22
+ ALTERNATE SOLUTION;
+ void srtf(TreeNode* root, string s, vector<string> &v){
+        if(!root) return;
+		
+        if(root->left==nullptr && root->right==nullptr){
+            s += to_string(root->val);
+            v.push_back(s);
+            return;
+        }
+        s += to_string(root->val);
+		
+        if(root->left) srtf(root->left, s, v);
+        if(root->right) srtf(root->right, s, v);
+        
+        
+    }
+    int sumRootToLeaf(TreeNode* root) {
+        if(!root) return 0;
+        string s = "";
+        vector<string> v;
+        srtf(root, s, v);
+        int ans =0;
+        for(int i=0;i<v.size();i++){
+            int num=0;
+            int x=0;
+            for(int j=v[i].size()-1;j>=0;j--){
+                if(v[i][j] == '1') num += pow(2,x);
+                x++;
+            }
+            ans += num;
+        }
+        
+        return ans;
+    }
+};
