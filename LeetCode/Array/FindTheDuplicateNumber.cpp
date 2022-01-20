4a213ae76c01e0301e53b329e951a7f2dec35e49
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-01-20 21:36:53 +0530

    LeetCodde

--- LeetCode/Array/FindTheDuplicateNumber.cpp
+++ LeetCode/Array/FindTheDuplicateNumber.cpp
@@ -20,13 +20,30 @@ int findDuplicate(vector<int> &nums)
     // }
     // return -1;
 
-    for (int i = 0; i < nums.size(); i++)
-    {
-        if (nums[abs(nums[i])] < 0)
-        {
-            return abs(nums[i]);
+    // for (int i = 0; i < nums.size(); i++)
+    // {
+    //     if (nums[abs(nums[i])] < 0)
+    //     {
+    //         return abs(nums[i]);
+    //     }
+    //     nums[abs(nums[i])] = -nums[abs(nums[i])];
+    // }
+    // return 0;
+        int slow=0;
+        int fast=0;
+        while(true){
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+            if(nums[slow]==nums[fast]){
+                break;
+            }
         }
-        nums[abs(nums[i])] = -nums[abs(nums[i])];
-    }
-    return 0;
+        fast=0;
+        while(nums[slow]!=nums[fast]){
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return nums[slow];
 }
+// Input: nums = [1,3,4,2,2]
+// Output: 2
