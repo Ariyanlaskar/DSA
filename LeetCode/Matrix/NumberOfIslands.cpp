d42271537e04037a5b826ed359eced094be38673
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-01-22 21:38:35 +0530

    LeetCode

---
+++ LeetCode/Matrix/NumberOfIslands.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int numIslands(vector<vector<char>> &grid)
+{
+    int count = 0;
+    for (int i = 0; i < grid.size(); i++)
+    {
+        for (int j = 0; j < grid[i].size(); j++)
+        {
+            if (grid[i][j] == '1')
+            {
+                count++;
+                dfs(i, j, grid);
+            }
+        }
+    }
+    return count;
+}
+void dfs(int i, int j, vector<vector<char>> &grid)
+{
+    if (i >= grid.size() || i < 0 || j < 0 || j >= grid[i].size() || grid[i][j] == '0')
+    {
+        return;
+    }
+    grid[i][j] = '0';
+    dfs(i + 1, j, grid);
+    dfs(i - 1, j, grid);
+    dfs(i, j + 1, grid);
+    dfs(i, j - 1, grid);
+}
