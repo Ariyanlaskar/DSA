72d4cbdc6ad11d8222a9ee0e20f2b91827c95a80
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-01-27 21:17:25 +0530

    LeetCode

---
+++ LeetCode/BinarySearch/SearchA2DMatrixII.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool searchMatrix(vector<vector<int>> &matrix, int target)
+{
+    int r = matrix.size();
+    int c = matrix[0].size();
+    int i = 0;
+    int j = c - 1;
+    while (i < r && j < c && j >= 0)
+    {
+        if (target == matrix[i][j])
+        {
+            return true;
+        }
+        else if (target > matrix[i][j])
+        {
+            i = i + 1;
+        }
+        else if (target < matrix[i][j])
+        {
+            j = j - 1;
+        }
+    }
+    return false;
+}
+// Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+// Output: true
