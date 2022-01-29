975627b6945bf82c21c8ef94dd1fbb1216ce780b
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-01-29 13:23:33 +0530

    LeetCode

---
+++ LeetCode/maths/CountPrimes.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int countPrimes(int n)
+{
+    if (n == 0 || n == 1 || n == 2)
+    {
+        return 0;
+    }
+    int countPrime = 0;
+    vector<bool> isPrime(n, true);
+    isPrime[0] = false;
+    isPrime[1] = false;
+    for (int i = 2; i * i < n; i++)
+    {
+        if (isPrime[i])
+        {
+            for (int j = i * i; j < n; j += i)
+            {
+                isPrime[j] = false;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (isPrime[i])
+        {
+            countPrime++;
+        }
+    }
+    return countPrime;
+}
