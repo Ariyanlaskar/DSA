90fd75ed02df453246011829b5e9be207877754f
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-02-03 13:01:46 +0530

    LeetCode

---
+++ LeetCode/Hashing/4SumII.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int fourSumCount(vector<int> &nums1, vector<int> &nums2, vector<int> &nums3, vector<int> &nums4)
+{
+    unordered_map<int, int> mp;
+    int count = 0;
+    for (auto &i : nums1)
+    {
+        for (auto &j : nums3)
+        {
+            mp[i + j]++;
+        }
+    }
+    for (auto &i : nums2)
+    {
+        for (auto &j : nums4)
+        {
+            count += mp[-(i + j)];
+        }
+    }
+    return count;
+}
