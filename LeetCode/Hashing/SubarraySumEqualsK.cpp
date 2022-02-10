ec638ea806c56ccbcea94d038df3b803023cde8c
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-02-10 22:22:02 +0530

    leetcode

--- LeetCode/Hashing/SubarraySumEqualsK.cpp
+++ LeetCode/Hashing/SubarraySumEqualsK.cpp
@@ -2,24 +2,18 @@
 using namespace std;
 int subarraySum(vector<int> &nums, int k)
 {
-    int Tsum = 0, ans = 0;
+    int sum = 0, ans = 0;
     unordered_map<int, int> mp;
     for (int i = 0; i < nums.size(); i++)
     {
-        mp[Tsum]++;
-        Tsum += nums[i];
-        if (mp[Tsum - k] != 0)
+        mp[sum]++;
+        sum += nums[i];
+        if (mp[(sum)-k] != 0)
         {
-            ans += mp[Tsum - k];
+            ans += mp[sum - k];
         }
     }
     return ans;
-}
-int main(){
-    vector<int> nums = {1,1,1};
-    int k = 2;
-    cout<<subarraySum(nums,k)<<endl;
-    return 0;
 }
 // Input: nums = [1,1,1], k = 2
 // Output: 2
