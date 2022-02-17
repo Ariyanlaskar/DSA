cd1d5c6a433e2b3157aaa025dfe6d351bff8ca6c
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-02-17 22:27:31 +0530

    LeetCod

---
+++ LeetCode/BinarySearch/KthSmallestElementInSortedMatrix.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+int kthSmallest(vector<vector<int>> &matrix, int k)
+{
+    int n = matrix.size();
+    int low = matrix[0][0], high = matrix[n - 1][n - 1];
+    int ans = 0;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (countMin(mid, matrix) >= k)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+int countMin(int m, vector<vector<int>> &matrix)
+{
+    int n = matrix.size();
+    int count = 0, row = 0, col = n - 1;
+    while (row <= n - 1 && col >= 0)
+    {
+        if (matrix[row][col] <= m)
+        {
+            count += col + 1;
+            row++;
+        }
+        else
+        {
+            col--;
+        }
+    }
+    return count;
+}
