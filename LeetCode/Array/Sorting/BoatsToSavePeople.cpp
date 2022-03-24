7ce777ac4b7691ee67251c56ce9fc12a0362d838
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-03-24 11:56:19 +0530

    LeetCode

---
+++ LeetCode/Array/Sorting/BoatsToSavePeople.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int numRescueBoats(vector<int> &people, int limit)
+{
+    sort(people.begin(), people.end());
+    int n = people.size();
+    int ans = 0;
+    int i = 0;
+    int j = n - 1;
+    while (i < j)
+    {
+        if (people[i] + people[j] <= limit)
+        {
+            ans++, i++, j--;
+        }
+        else if (people[i] + people[j] > limit)
+        {
+            ans++, j--;
+        }
+    }
+    if (i == j)
+    {
+        ans++;
+    }
+    return ans;
+}
+// Input: people = [3,2,2,1], limit = 3
+// Output: 3
