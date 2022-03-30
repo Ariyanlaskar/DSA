27111482e717ad1c297557a9b8c2bfcff38975f7
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-03-30 20:31:35 +0530

    LeetCode

---
+++ LeetCode/DP/WordBreak.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+unordered_map<string, int> st;
+unordered_map<int, bool> mp;
+bool wordBreak(string s, vector<string> &wordDict)
+{
+    for (int i = 0; i < wordDict.size(); i++)
+    {
+        st[wordDict[i]]++;
+    }
+    return solve(s, 0);
+}
+bool solve(string s, int pos)
+{
+    if (pos == s.size())
+    {
+        return true;
+    }
+    if (mp.count(pos))
+    {
+        return mp[pos];
+    }
+    string temp = "";
+    for (int i = pos; i < s.size(); i++)
+    {
+        temp = s.substr(pos, i - pos + 1);
+        if (st.count(temp) && solve(s, i + 1))
+        {
+            return mp[pos] = true;
+        }
+    }
+    return mp[pos] = false;
+}
+// Input: s = "leetcode", wordDict = ["leet","code"]
+// Output: true
