972e3160e57099ba202a09a8deac35c39ee72b2e
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-04-13 21:45:41 +0530

    LeetCode

--- LeetCode/Matrix/SpiralMatrixII.cpp
+++ LeetCode/Matrix/SpiralMatrixII.cpp
@@ -1,30 +1,36 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-vector<vector<int>> generateMatrix(int n) {
-        vector<vector<int>>matrix(n,vector<int>(n,0));
-        int c=1;
-        int colStart=0,colEnd=n-1,rowStart=0,rowEnd=n-1;
-        while(colStart<=colEnd && rowStart<=rowEnd){
-            for(int col=colStart;col<=colEnd;col++){
-                matrix[rowStart][col]=c;
-                c++;
-            }
-            rowStart++;
-            for(int row=rowStart;row<=rowEnd;row++){
-                matrix[row][colEnd]=c;
-                c++;
-            }
-            colEnd--;
-            for(int col=colEnd;col>=colStart;col--){
-                matrix[rowEnd][col]=c;
-                c++;
-            }
-            rowEnd--;
-            for(int row=rowEnd;row>=rowStart;row--){
-                matrix[row][colStart]=c;
-                c++;
-            }
-            colStart++;
+vector<vector<int>> generateMatrix(int n)
+{
+    vector<vector<int>> matrix(n, vector<int>(n, 0));
+    int c = 1;
+    int colStart = 0, colEnd = n - 1, rowStart = 0, rowEnd = n - 1;
+    while (colStart <= colEnd && rowStart <= rowEnd)
+    {
+        for (int col = colStart; col <= colEnd; col++)
+        {
+            matrix[rowStart][col] = c;
+            c++;
         }
-        return matrix;
+        rowStart++;
+        for (int row = rowStart; row <= rowEnd; row++)
+        {
+            matrix[row][colEnd] = c;
+            c++;
+        }
+        colEnd--;
+        for (int col = colEnd; col >= colStart; col--)
+        {
+            matrix[rowEnd][col] = c;
+            c++;
+        }
+        rowEnd--;
+        for (int row = rowEnd; row >= rowStart; row--)
+        {
+            matrix[row][colStart] = c;
+            c++;
+        }
+        colStart++;
     }
+    return matrix;
+}
