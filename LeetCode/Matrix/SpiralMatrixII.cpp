9953079688a51b4aec8a55ac244f51fb7e4a713f
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-04-13 21:44:11 +0530

    LeetCode

--- LeetCode/Matrix/SpiralMatrixII.cpp
+++ LeetCode/Matrix/SpiralMatrixII.cpp
@@ -1,38 +1,30 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-vector<vector<int>> generateMatrix(int n)
-{
-    vector<vector<int>> matrix2(n, vector<int>(n, 0));
-    int c = 1;
-    int colStart = 0, rowStart = 0, colEnd = n - 1, rowEnd = n - 1;
-    while (colStart <= colEnd && rowStart <= rowEnd)
-    {
-        for (int col = colStart; col <= colEnd; col++)
-        {
-            matrix2[rowStart][col] = c;
-            c++;
+vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>>matrix(n,vector<int>(n,0));
+        int c=1;
+        int colStart=0,colEnd=n-1,rowStart=0,rowEnd=n-1;
+        while(colStart<=colEnd && rowStart<=rowEnd){
+            for(int col=colStart;col<=colEnd;col++){
+                matrix[rowStart][col]=c;
+                c++;
+            }
+            rowStart++;
+            for(int row=rowStart;row<=rowEnd;row++){
+                matrix[row][colEnd]=c;
+                c++;
+            }
+            colEnd--;
+            for(int col=colEnd;col>=colStart;col--){
+                matrix[rowEnd][col]=c;
+                c++;
+            }
+            rowEnd--;
+            for(int row=rowEnd;row>=rowStart;row--){
+                matrix[row][colStart]=c;
+                c++;
+            }
+            colStart++;
         }
-        rowStart++;
-        for (int row = rowStart; row <= rowEnd; row++)
-        {
-            matrix2[row][colEnd] = c;
-            c++;
-        }
-        colEnd--;
-        for (int col = colEnd; col >= colStart; col--)
-        {
-            matrix2[rowEnd][col] = c;
-            c++;
-        }
-        rowEnd--;
-        for (int row = rowEnd; row >= rowStart; row--)
-        {
-            matrix2[row][colStart] = c;
-            c++;
-        }
-        colStart++;
+        return matrix;
     }
-    return matrix2;
-}
-// Input: n = 3
-// Output: [[1,2,3],[8,9,4],[7,6,5]]
