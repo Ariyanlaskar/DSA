bd379b1497f15c072932566f0974c9b154fce016
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-04-16 22:08:43 +0530

    LeetCode

--- LeetCode/BInaryTree&BST/ConvertBSTToGreaterTree.cpp
+++ LeetCode/BInaryTree&BST/ConvertBSTToGreaterTree.cpp
@@ -21,3 +21,6 @@ TreeNode *convertBST(TreeNode *root)
 }
 // Input: root = [4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]
 // Output: [30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]
+
+// Input: root = [0,null,1]
+// Output: [1,null,1]
