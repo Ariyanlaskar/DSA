6a83c987c941f68fa29b1f55a5879cad816fe1e0
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-04-24 11:58:28 +0530

    LeetCode

--- LeetCode/Hashing/DesignUndergroundSystem.cpp
+++ LeetCode/Hashing/DesignUndergroundSystem.cpp
@@ -33,3 +33,18 @@ double getAverageTime(string startStation, string endStation)
 
 // Output
 // [null,null,null,null,null,null,null,14.00000,11.00000,null,11.00000,null,12.00000]
+
+// Explanation
+// UndergroundSystem undergroundSystem = new UndergroundSystem();
+// undergroundSystem.checkIn(45, "Leyton", 3);
+// undergroundSystem.checkIn(32, "Paradise", 8);
+// undergroundSystem.checkIn(27, "Leyton", 10);
+// undergroundSystem.checkOut(45, "Waterloo", 15);  // Customer 45 "Leyton" -> "Waterloo" in 15-3 = 12
+// undergroundSystem.checkOut(27, "Waterloo", 20);  // Customer 27 "Leyton" -> "Waterloo" in 20-10 = 10
+// undergroundSystem.checkOut(32, "Cambridge", 22); // Customer 32 "Paradise" -> "Cambridge" in 22-8 = 14
+// undergroundSystem.getAverageTime("Paradise", "Cambridge"); // return 14.00000. One trip "Paradise" -> "Cambridge", (14) / 1 = 14
+// undergroundSystem.getAverageTime("Leyton", "Waterloo");    // return 11.00000. Two trips "Leyton" -> "Waterloo", (10 + 12) / 2 = 11
+// undergroundSystem.checkIn(10, "Leyton", 24);
+// undergroundSystem.getAverageTime("Leyton", "Waterloo");    // return 11.00000
+// undergroundSystem.checkOut(10, "Waterloo", 38);  // Customer 10 "Leyton" -> "Waterloo" in 38-24 = 14
+// // undergroundSystem.getAverageTime("Leyton", "Waterloo");    // return 12.00000. Three trips "Leyton" -> "Waterloo", (10 + 12 + 14) / 3 = 12
