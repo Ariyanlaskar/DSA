83fee009d7f266e1ba70fb8d0075c6ed82ae3928
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-05-05 21:54:00 +0530

    LeetCode

--- LeetCode/Stack&Queue/ImplementStackusingQueues.cpp
+++ LeetCode/Stack&Queue/ImplementStackusingQueues.cpp
@@ -49,10 +49,3 @@ bool empty()
 // Output
 // [null, null, null, 2, 2, false]
 
-// Explanation
-// MyStack myStack = new MyStack();
-// myStack.push(1);
-// myStack.push(2);
-// myStack.top(); // return 2
-// myStack.pop(); // return 2
-// myStack.empty(); // return False
