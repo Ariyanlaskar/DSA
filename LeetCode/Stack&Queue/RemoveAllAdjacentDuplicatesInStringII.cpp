d68814a4a55c777c11cdfd5920a04a57ec5fb57e
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-05-06 15:47:21 +0530

    LeetCode

---
+++ LeetCode/Stack&Queue/RemoveAllAdjacentDuplicatesInStringII.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+string removeDuplicates(string s, int k)
+{
+    int n = s.size();
+    if (n < k)
+    {
+        return s;
+    }
+    stack<pair<char, int>> st;
+    for (int i = 0; i < n; i++)
+    {
+        if (st.empty() || st.top().first != s[i])
+        {
+            st.push({s[i], 1});
+        }
+        else
+        {
+            auto prev = st.top().second;
+            st.pop();
+            st.push({s[i], prev + 1});
+        }
+        if (st.top().second == k)
+        {
+            st.pop();
+        }
+    }
+    string ans = "";
+    while (!st.empty())
+    {
+        for (int i = 0; i < st.top().second; i++)
+        {
+            ans += st.top().first;
+        }
+        st.pop();
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
