9f7f7361cad46c95c567f588c7edb1ab0b5e0ce1
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-05-07 11:29:56 +0530

    LeetCode

--- LeetCode/Stack&Queue/132Pattern.cpp
+++ LeetCode/Stack&Queue/132Pattern.cpp
@@ -21,3 +21,7 @@ bool find132pattern(vector<int> &nums)
 }
 // Input: nums = [1,2,3,4]
 // Output: false
+
+// Input: nums = [3,1,4,2]
+// Output: true
+// Explanation: There is a 132 pattern in the sequence: [1, 4, 2].
