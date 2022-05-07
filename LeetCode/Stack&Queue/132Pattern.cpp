28986e6e0a8f894b23a32ab72e5ae159c0974fb2
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-05-07 10:37:18 +0530

    LeetCode

---
+++ LeetCode/Stack&Queue/132Pattern.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool find132pattern(vector<int> &nums)
+{
+    stack<int> st;
+    int mini = INT_MIN;
+    for (int i = nums.size() - 1; i >= 0; i--)
+    {
+        if (nums[i] < mini)
+        {
+            return true;
+        }
+        while (!st.empty() && st.top() < nums[i])
+        {
+            mini = st.top();
+            st.pop();
+        }
+        st.push(nums[i]);
+    }
+    return false;
+}
