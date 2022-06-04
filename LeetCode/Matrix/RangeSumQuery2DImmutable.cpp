8c1fcc64de2f8ca899780e1cfa49925550e7a00a
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-06-04 13:15:40 +0530

    LeetCode

--- LeetCode/Matrix/RangeSumQuery2DImmutable.cpp
+++ LeetCode/Matrix/RangeSumQuery2DImmutable.cpp
@@ -40,6 +40,7 @@ int sumRegion(int row1, int col1, int row2, int col2)
     }
     return sum;
 }
+
 // Input
 // ["NumMatrix", "sumRegion", "sumRegion", "sumRegion"]
 // [[[[3, 0, 1, 4, 2], [5, 6, 3, 2, 1], [1, 2, 0, 1, 5], [4, 1, 0, 1, 7], [1, 0, 3, 0, 5]]], [2, 1, 4, 3], [1, 1, 2, 2], [1, 2, 2, 4]]
