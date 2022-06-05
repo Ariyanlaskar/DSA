a956a5841ad2dc13af5eac3aace13ffe44917f65
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-06-05 19:58:14 +0530

    LeetCode

--- LeetCode/Recursion&Backtracking/N-Queens-II.cpp
+++ LeetCode/Recursion&Backtracking/N-Queens-II.cpp
@@ -1,7 +1,35 @@
-//LeetCode Hard 52;
+// LeetCode Hard 52;
 #include <bits/stdc++.h>
 using namespace std;
-vector<vector<string>> solvedQueens;
+vector<vector<string>> QueenBoard;
+int totalNQueens(int n)
+{
+    vector<string> board(n, string(n, '.'));
+    findSol(board, 0, n);
+    int ans = QueenBoard.size();
+    return ans;
+}
+void findSol(vector<string> &board, int row, int queens)
+{
+    if (row == board.size())
+    {
+        if (queens == 0)
+        {
+            QueenBoard.push_back(board);
+        }
+        return;
+    }
+    for (int col = 0; col < board.size(); col++)
+    {
+        if (isSafe(board, row, col))
+        {
+            board[row][col] = 'Q';
+            findSol(board, row + 1, queens - 1);
+            board[row][col] = '.';
+        }
+    }
+    return;
+}
 bool isSafe(vector<string> &board, int row, int col)
 {
     for (int r = 0; r < row; r++)
@@ -18,53 +46,17 @@ bool isSafe(vector<string> &board, int row, int col)
         {
             return false;
         }
-        r--;
-        c--;
+        r--, c--;
     }
     r = row - 1, c = col + 1;
-    while (r >= 0 && col < board.size())
+    while (r >= 0 && c < board.size())
     {
         if (board[r][c] == 'Q')
         {
             return false;
         }
-        r--;
-        c++;
+        r--, c++;
     }
     return true;
 }
-void solver(vector<string> &board, int row, int queens)
-{
-    if (row == board.size())
-    {
-        if (queens == 0)
-        {
-            solvedQueens.push_back(board);
-        }
-        return;
-    }
-    for (int col = 0; col < board.size(); col++)
-    {
-        if (isSafe(board, row, col))
-        {
-            board[row][col] = 'Q';
-            solver(board, row + 1, queens - 1);
-            board[row][col] = '.';
-        }
-    }
-    return;
-}
-int totalNQueens(int n)
-{
-    vector<string> board(n, string(n, '.'));
-    solver(board, 0, n);
-    int ans = solvedQueens.size();
-    return ans;
-}
-int main()
-{
-    int n=4;
-    cout<<totalNQueens(n)<<endl;
-    return 0;
-}
-// Output : 2; 
+// Output : 2;
