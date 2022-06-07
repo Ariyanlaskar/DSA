65d8acc6915db61c238964a34ac38088034f8968
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-06-07 21:16:40 +0530

    LeetCode

--- LeetCode/Array/MergeSortedArray.cpp
+++ LeetCode/Array/MergeSortedArray.cpp
@@ -24,3 +24,4 @@ void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
 }
 // Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
 // Output: [1,2,2,3,5,6]
+// Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
