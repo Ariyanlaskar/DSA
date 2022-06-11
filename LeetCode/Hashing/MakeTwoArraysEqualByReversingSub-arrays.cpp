b26d64351d81281792abbae0b187dd82a7f45d10
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-06-11 12:17:13 +0530

    LeetCode

---
+++ LeetCode/Hashing/MakeTwoArraysEqualByReversingSub-arrays.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool canBeEqual(vector<int> &target, vector<int> &arr)
+{
+    unordered_map<int, int> mp;
+    for (auto i : target)
+    {
+        mp[i]++;
+    }
+    for (auto j : arr)
+    {
+        if (mp[j] == 0)
+        {
+            return false;
+        }
+        mp[j]--;
+    }
+    return true;
+}
+// Input: target = [1,2,3,4], arr = [2,4,1,3]
+// Output: true
