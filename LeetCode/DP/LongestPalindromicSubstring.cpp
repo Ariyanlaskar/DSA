78fa3e476301db6e88c3791826831f84a20a90ef
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-06-16 16:52:19 +0530

    LeetCode

--- LeetCode/DP/LongestPalindromicSubstring.cpp
+++ LeetCode/DP/LongestPalindromicSubstring.cpp
@@ -1,40 +1,56 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-string LPS(string s){
-    int n=s.length();
-    vector<vector<bool>>dp(n,vector<bool>(n,false));
-    int start,len;
-    for(int g=0;g<n;g++){
-        for(int i=0,j=g;j<n;i++,j++){
-            if(g==0){
-                dp[i][j]=true;
+
+string LPS(string s)
+{
+    int n = s.size();
+    vector<vector<bool>> dp(n, vector<bool>(n, false));
+    int start, len;
+    for (int k = 0; k < n; k++)
+    {
+        int i = 0, j = k;
+        while (j < n)
+        {
+            if (k == 0)
+            {
+                dp[i][j] = true;
             }
-            else if(g==1){
-                if(s[i]==s[j]){
-                    dp[i][j]=true;
+            else if (k == 1)
+            {
+                if (s[i] == s[j])
+                {
+                    dp[i][j] = true;
                 }
-                else{
-                    dp[i][j]=false;
-                }
-            }
-            else{
-                if(s[i]==s[j] && dp[i+1][j-1]){
-                    dp[i][j]=true;
-                }
-                else{
-                    dp[i][j]=false;
+                else
+                {
+                    dp[i][j] = false;
                 }
             }
-            if(dp[i][j]){
-                start=i;
-                len=j-i+1;
+            else
+            {
+                if (s[i] == s[j] && dp[i + 1][j - 1])
+                {
+                    dp[i][j] = true;
+                }
+                else
+                {
+                    false;
+                }
             }
+            if (dp[i][j])
+            {
+                start = i;
+                len = j - i + 1;
+            }
+            i++;
+            j++;
         }
     }
-    return s.substr(start,len);
+    return s.substr(start, len);
 }
-int main(){
-    string s="bb";
-    cout<<LPS(s)<<endl;
+int main()
+{
+    string s = "babdc";
+    cout << LPS(s) << endl;
     return 0;
 }
