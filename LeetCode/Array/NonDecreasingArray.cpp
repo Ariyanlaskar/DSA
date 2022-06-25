7211fd4e55d5cc4243a2486db8db2cbd6f0b2cae
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-06-25 23:57:35 +0530

    LeetCode

---
+++ LeetCode/Array/NonDecreasingArray.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool checkPossibility(vector<int> &nums)
+{
+    bool turn = false;
+    int n = nums.size();
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (nums[i] > nums[i + 1] && i - 1 < 0)
+        {
+            turn = true;
+        }
+        else if (nums[i] > nums[i + 1])
+        {
+            if (i == n - 2 && turn == false)
+            {
+                turn == false;
+            }
+            else if (nums[i - 1] <= nums[i + 1] && turn == false)
+            {
+                turn = true;
+            }
+            else if (nums[i] <= nums[i + 2] && turn == false)
+            {
+                turn = true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
