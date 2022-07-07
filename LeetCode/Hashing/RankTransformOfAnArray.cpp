49ad939d4ff4bff6566b238c670629c3fe330bb2
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-07-07 16:32:02 +0530

    LeetCode

--- LeetCode/Hashing/RankTransformOfAnArray.cpp
+++ LeetCode/Hashing/RankTransformOfAnArray.cpp
@@ -23,3 +23,4 @@ vector<int> arrayRankTransform(vector<int> &arr)
 
 // Input: arr = [40,10,20,30]
 // Output: [4,1,2,3]
+// Explanation: 40 is the largest element. 10 is the smallest. 20 is the second smallest. 30 is the third smallest.
