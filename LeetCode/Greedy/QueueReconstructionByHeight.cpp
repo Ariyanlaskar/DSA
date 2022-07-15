41796bc8421f53e5d4d06a684a6decf4195c9bfb
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-07-15 23:46:01 +0530

    LeetCode

--- LeetCode/Greedy/QueueReconstructionByHeight.cpp
+++ LeetCode/Greedy/QueueReconstructionByHeight.cpp
@@ -24,3 +24,5 @@ vector<vector<int>> reconstructQueue(vector<vector<int>> &people)
     }
     return ans;
 }
+// Input: people = [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]
+// Output: [[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]]
