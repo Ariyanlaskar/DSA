d06cff290a123d2d12e2e2a0ced1df477cd56e66
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-07-21 23:26:41 +0530

    LeetCode

---
+++ LeetCode/String/MinimumChangesToMakeAlternatingBinaryString.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int minOperations(string s)
+{
+    int prev = s[0];
+    int n = s.size();
+    int count = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (s[i] == '0' && s[i] == prev)
+        {
+            s[i] = '1';
+            prev = s[i];
+            count++;
+        }
+        else if (s[i] == '1' && s[i] == prev)
+        {
+            s[i] = '0';
+            prev = s[i];
+            count++;
+        }
+        else
+        {
+            prev = s[i];
+        }
+    }
+    if (n - count < count)
+    {
+        return n - count;
+    }
+    return count;
+}
+// Input: s = "0100"
+// Output: 1
