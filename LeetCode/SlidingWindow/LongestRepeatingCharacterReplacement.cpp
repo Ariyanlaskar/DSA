7c47e9379a3c1b140834be509c680c4fb4bdedf0
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-08-21 23:01:24 +0530

    LeetCod

---
+++ LeetCode/SlidingWindow/LongestRepeatingCharacterReplacement.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int characterReplacement(string s, int k)
+{
+    int i = 0, j = 0, maxCharCount = 0;
+    int count[26] = {0};
+    int n = s.length();
+    int maxLength = 0;
+    while (j < n)
+    {
+        count[s[j] - 'A']++;
+        maxCharCount = max(maxCharCount, count[s[j] - 'A']);
+        if (j - i + 1 - maxCharCount > k)
+        {
+            count[s[i] - 'A']--;
+            i++;
+        }
+
+        maxLength = max(maxLength, j - i + 1);
+        j++;
+    }
+
+    return maxLength;
+}
+// Input: s = "ABAB", k = 2
+// Output: 4
