223fd375d7a75975c91ff41ce2fe1dda9cc95b15
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-08-22 13:12:50 +0530

    LeetCod

---
+++ LeetCode/Array/PowerOfFour.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool isPowerOfFour(int n)
+{
+    while (n > 0 && n % 4 == 0)
+    {
+        n /= 4;
+    }
+    if (n == 1)
+    {
+        return true;
+    }
+    return false;
+}
+// Input: n = 16
+// Output: true
