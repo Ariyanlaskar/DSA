cd8ee0a67de108baef103181d6330caf7f89f24f
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-09-10 21:16:29 +0530

    LeetCode

--- LeetCode/Recursion&Backtracking/CombinationSum.cpp
+++ LeetCode/Recursion&Backtracking/CombinationSum.cpp
@@ -1,28 +1,28 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-void solve(int idx,vector<int>&candidates,vector<vector<int>>&v,vector<int>&ans,int target){
-    if(idx==candidates.size()){
-        if(target==0){
-            v.push_back(ans);
+vector<vector<int>> res;
+vector<int> ans;
+vector<vector<int>> combinationSum(vector<int> &candidates, int target)
+{
+    findComb(candidates, target, 0);
+    return res;
+}
+void findComb(vector<int> &candidates, int target, int idx)
+{
+    if (idx == candidates.size())
+    {
+        if (target == 0)
+        {
+            res.push_back(ans);
         }
         return;
     }
-    if(candidates[idx]<=target){
+    if (candidates[idx] <= target)
+    {
         ans.push_back(candidates[idx]);
-        solve(idx,candidates,v,ans,target-candidates[idx]);
+        findComb(candidates, target - candidates[idx], idx);
         ans.pop_back();
     }
-    solve(idx+1,candidates,v,ans,target);
-}
-vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-    vector<vector<int>>v;
-    vector<int>ans;
-    solve(0,candidates,v,ans,target);
-    return v;
-}
-int main(){
-    vector<int>candidates = {2,3,5};
-    int target=8;
-    return 0;
+    findComb(candidates, target, idx + 1);
 }
 // Output = [[2,2,2,2],[2,3,3],[3,5]];
