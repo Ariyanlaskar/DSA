457d649938ce474c601fc564aca3edd2cb75ad30
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-09-17 21:00:30 +0530

    LeetCode

--- LeetCode/Hashing/LongestSubstringWithoutRepeatingCharacters.cpp
+++ LeetCode/Hashing/LongestSubstringWithoutRepeatingCharacters.cpp
@@ -23,4 +23,3 @@ int lengthOfLongestSubstring(string s)
 // Input: s = "pwwkew"
 // Output: 3
 // Explanation: The answer is "wke", with the length of 3.
-// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
