87013e3af634b8c0821706c8fe7575998dfa6f18
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-09-20 21:25:12 +0530

    LeetCode

--- LeetCode/DP/MaximumLengthOfRepeatedSubarray.cpp
+++ LeetCode/DP/MaximumLengthOfRepeatedSubarray.cpp
@@ -1,21 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-  int findLength(vector<int>& nums1, vector<int>& nums2) {
-        int n=nums1.size(),m=nums2.size();
-        vector<vector<int>>dp(n,vector<int>(m,0));
-        int count=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(nums1[i]==nums2[j]){
-                    if(i==0 || j==0){
-                        dp[i][j]=1;
-                    }
-                    else{
-                        dp[i][j]=dp[i-1][j-1] + 1;
-                    }
-                    count=max(dp[i][j],count);
+int findLength(vector<int> &nums1, vector<int> &nums2)
+{
+    int n = nums1.size(), m = nums2.size();
+    vector<vector<int>> dp(n, vector<int>(m, 0));
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (nums1[i] == nums2[j])
+            {
+                if (i == 0 || j == 0)
+                {
+                    dp[i][j] = 1;
                 }
+                else
+                {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                count = max(dp[i][j], count);
             }
         }
-        return count;
     }
+    return count;
+}
