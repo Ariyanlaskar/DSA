90038930b17b97d35db9a3ad4e377ca64512d587
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-09-30 00:15:40 +0530

    LeetCode

---
+++ LeetCode/PriorityQueue/FindKClosestElements.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> findClosestElements(vector<int> &arr, int k, int x)
+{
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    vector<int> ans;
+    for (auto &i : arr)
+    {
+        int diff = abs(i - x);
+        pq.push({diff, i});
+    }
+    while (k > 0)
+    {
+        ans.push_back(pq.top().second);
+        pq.pop();
+        k--;
+    }
+    sort(ans.begin(), ans.end());
+    return ans;
+}
+// Input: arr = [1,2,3,4,5], k = 4, x = 3
+// Output: [1,2,3,4]
