d801e7622a47abf317dfee2222f32225d456ea3b
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-10-04 20:01:46 +0530

    LeetCode

--- LeetCode/BInaryTree&BST/pathSumIII.cpp
+++ LeetCode/BInaryTree&BST/pathSumIII.cpp
@@ -26,31 +26,32 @@ void CountPathSum(TreeNode *root, int targetSum, long sum)
     mp[sum]--;
 }
 // Without Using HashMap;
-int count = 0;
-int pathSum(TreeNode *root, int targetSum)
-{
-    if (root == NULL)
-    {
-        return 0;
-    }
-    dfs(root, targetSum);
-    pathSum(root->left, targetSum);
-    pathSum(root->right, targetSum);
-    return count;
-}
-void dfs(TreeNode *root, int targetSum)
-{
-    if (root == NULL)
-    {
-        return;
-    }
-    targetSum = targetSum - root->val;
-    if (targetSum == 0)
-    {
-        count++;
-    }
-    dfs(root->left, targetSum);
-    dfs(root->right, targetSum);
-}
+
+// int count = 0;
+// int pathSum(TreeNode *root, int targetSum)
+// {
+//     if (root == NULL)
+//     {
+//         return 0;
+//     }
+//     dfs(root, targetSum);
+//     pathSum(root->left, targetSum);
+//     pathSum(root->right, targetSum);
+//     return count;
+// }
+// void dfs(TreeNode *root, int targetSum)
+// {
+//     if (root == NULL)
+//     {
+//         return;
+//     }
+//     targetSum = targetSum - root->val;
+//     if (targetSum == 0)
+//     {
+//         count++;
+//     }
+//     dfs(root->left, targetSum);
+//     dfs(root->right, targetSum);
+// }
 // Input: root = [10,5,-3,3,2,null,11,3,-2,null,1], targetSum = 8
 // Output: 3
