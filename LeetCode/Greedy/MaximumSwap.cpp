05ef2477d75e1a68f2929bf019512399ea57a9ec
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-10-13 21:20:30 +0530

    LeetCode

---
+++ LeetCode/Greedy/MaximumSwap.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int maximumSwap(int num)
+{
+    string nums = to_string(num);
+    int count = 0, maxValue, idx = -1;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        maxValue = nums[i];
+        for (int j = i + 1; j < nums.size(); j++)
+        {
+            if (maxValue <= nums[j])
+            {
+                maxValue = nums[j];
+                idx = j;
+            }
+        }
+        if (count == 0 && idx != -1 && nums[i] != maxValue)
+        {
+            swap(nums[i], nums[idx]);
+            count++;
+        }
+    }
+    return stoi(nums);
+}
