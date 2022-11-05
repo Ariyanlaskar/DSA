ac75abfb528a20ce9e18b32f1db5ec2cbc516097
Ariyan Laskar <ariyanlaskar2002@gmail.com>
2022-11-05 22:46:25 +0530

    LeetCod

--- LeetCode/Graph/841KeysAndRooms.cpp
+++ LeetCode/Graph/841KeysAndRooms.cpp
@@ -1,27 +1,29 @@
 // LeetCode Medium 841
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-void dfs(int node,vector<vector<int>>&rooms,vector<bool>&vis){
-    vis[node]=true;
-    for(auto it:rooms[node]){
-        if(!vis[it]){
-            dfs(it,rooms,vis);
+void dfs(int node, vector<vector<int>> &rooms, vector<bool> &vis)
+{
+    vis[node] = true;
+    for (auto it : rooms[node])
+    {
+        if (!vis[it])
+        {
+            dfs(it, rooms, vis);
         }
     }
 }
-bool canVisitAllRooms(vector<vector<int>>& rooms) {
-    int n=rooms.size();
-    vector<bool>vis(n,false);
-    dfs(0,rooms,vis);
-    for(int i=0;i<n;i++){
-        if(vis[i]==false)
+bool canVisitAllRooms(vector<vector<int>> &rooms)
+{
+    int n = rooms.size();
+    vector<bool> vis(n, false);
+    dfs(0, rooms, vis);
+    for (int i = 0; i < n; i++)
+    {
+        if (vis[i] == false)
             return false;
     }
     return true;
 }
-int main(){
-    vector<vector<int>>rooms = {{1},{2},{3},{}};
-    //sample test cases-> rooms = {{1,3},{3,0,1},{2},{0}};
-    cout<<canVisitAllRooms(rooms)<<endl;
-    return 0;
-}
+
+// Input: rooms = [[1],[2],[3],[]]
+// Output: true
